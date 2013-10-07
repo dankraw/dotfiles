23862ae6ef1e740f186523dbdea3e61926a69467
dankraw <dankraw@gmail.com>
2013-10-07 14:58:43 +0200

    install script improvements: default values & fancy colors

--- install.sh
+++ install.sh
@@ -2,31 +2,47 @@
 
 DOTFILES=`pwd`
 
-install_git () {
-    read -p "Your git user.name: " USER_NAME
-    read -p "Your git user.email: " USER_EMAIL
+# Colors
+ESC_SEQ="\x1b["
+RESET=$ESC_SEQ"39;49;00m"
+RED=$ESC_SEQ"31;01m"
+GREEN=$ESC_SEQ"32;01m"
+YELLOW=$ESC_SEQ"33;01m"
+BLUE=$ESC_SEQ"34;01m"
+MAGENTA=$ESC_SEQ"35;01m"
+CYAN=$ESC_SEQ"36;01m"
 
-    GIT_CONFIG=$HOME/.gitconfig
-    cp gitconfig $GIT_CONFIG
-    sed -i.bak s/%USER_NAME%/"$USER_NAME"/g $GIT_CONFIG
-    sed -i.bak s/%USER_EMAIL%/"$USER_EMAIL"/g $GIT_CONFIG
+install_git () {
+    echo -e "${BLUE}Installing git${RESET}"
+
+    USER_NAME=`git config --global user.name`
+    read -e -p "Your name: " -i "$USER_NAME" USER_NAME
+
+    USER_EMAIL=`git config --global user.email`
+    read -e -p "Your email: " -i "$USER_EMAIL" USER_EMAIL
     
-    rm "${GIT_CONFIG}.bak"
+    CONFIG=$HOME/.gitconfig
+    cp gitconfig $CONFIG
+    sed -i.bak s/%USER_NAME%/"$USER_NAME"/g $CONFIG
+    sed -i.bak s/%USER_EMAIL%/"$USER_EMAIL"/g $CONFIG
+    
+    rm "${CONFIG}.bak"
 }
 
 install_screen () {
-   ln -s $DOTFILES/screenrc $HOME/.screenrc
+   echo -e "${BLUE}Installing screen${RESET}"
+   ln -sf $DOTFILES/screenrc $HOME/.screenrc
 }
 
 install () {
    install_git
    install_screen 
-   echo "Done"
+   echo -e "${GREEN}Done${RESET}"
 }
 
 echo "Do you wish to install dotfiles?"
-echo "Warning: This may override dotfiles existing in your $HOME directory!"
-read -p "(Y/n) "  RESP
+echo -e "${RED}Warning: This may override dotfiles existing in your $HOME directory!${RESET}"
+read -p "(Y/n): "  RESP
 if [ "$RESP" = "Y" ]; then
     install
 fi    
