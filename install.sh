e24858bfb24dc364ce87683df7c04150bb0db9df
Daniel Krawczyk <daniel.krawczyk@allegro.pl>
2019-02-26 16:36:18 +0100

    Install fixes, bashrc

--- install.sh
+++ install.sh
@@ -12,9 +12,37 @@ BLUE=$ESC_SEQ"34;01m"
 MAGENTA=$ESC_SEQ"35;01m"
 CYAN=$ESC_SEQ"36;01m"
 
+link_file () {
+    src=$1
+    dst=$2
+    dst_dir=`dirname "$dst"`
+    mkdir -p $dst_dir
+    if [ -e "$dst" ]; then
+        if [ "$(readlink $dst)" = "$src" ]; then
+            echo "$src is already linked to $dst"
+        else
+            read -p "$dst already exist. Replace it (y/[N])? " answer
+            case ${answer:0:1} in
+                y|Y )
+                    echo "Removing $dst"
+                    rm "$dst"
+                    echo "Linking $src to $dst"
+                    ln -s "$src" "$dst"
+                ;;
+                * )
+                    echo "$dst not replaced"
+                ;;
+            esac
+        fi
+    else
+        echo "Linking $src to $dst"
+        ln -s "$src" "${dst}"
+    fi
+}
+
+
 install_git () {
     echo -e "${BLUE}Installing git${RESET}"
-
     USER_NAME=`git config --global user.name`
     if [ "$USER_NAME" = "" ]; then
         read -e -p "Your name: " USER_NAME
@@ -35,32 +63,31 @@ install_git () {
 
 install_git_templates() {
     echo -e "${BLUE}Installing git templates${RESET}"
-    ln -sf $DOTFILES/git-templates $HOME/.git-templates
+    link_file $DOTFILES/git-templates/ $HOME/.git-templates
     git config --global init.templatedir $HOME/.git-templates
 }
 
 install_screen () {
     echo -e "${BLUE}Installing screen${RESET}"
-    ln -sf $DOTFILES/screenrc $HOME/.screenrc
+    link_file $DOTFILES/screenrc $HOME/.screenrc
 }
 
 install_vim () {
     echo -e "${BLUE}Installing vim${RESET}"
-    ln -sf $DOTFILES/vimrc $HOME/.vimrc
+    link_file $DOTFILES/vimrc $HOME/.vimrc
+}
+
+install_bash () {
+    echo -e "${BLUE}Installing bash${RESET}"
+    link_file $DOTFILES/bashrc $HOME/.bashrc
 }
 
 install () {
    install_git
    install_git_templates
-   install_screen 
+   install_screen
    install_vim
-   echo -e "${GREEN}Done${RESET}"
+   install_bash
 }
 
-echo "Do you wish to install dotfiles?"
-echo -e "${RED}Warning: This may override dotfiles existing in your $HOME directory!${RESET}"
-read -p "(Y/n): "  RESP
-if [ "$RESP" = "Y" ]; then
-    install
-fi    
-
+install
